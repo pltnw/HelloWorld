d315bcdb81a16c2ffc15815138299d2c8ce9f896
pltnw <k.pltnw@gmail.com>
2022-09-14 20:22:30 +0200

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Екатерина Платонова on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
